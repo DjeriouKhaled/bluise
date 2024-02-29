25268b62b89e10d7de464151efbb4da3a6c181cf
Khaled <61236213+DjeriouKhaled@users.noreply.github.com>
2024-02-29 13:48:42 +0000

    Update index.vue
    fgggggggg

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,35 +4,15 @@
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
 
-        <div class="mb-12 xl:mb-0">
-          <h4 v-if="isSignedUp">Thank you - we'll be in touch shortly.</h4>
-
-          <form
-            v-else
-            @submit.prevent="handleSubmit"
-            name="signups"
-            netlify
-            class="flex items-center border-b border-b-2 border-blue-400 py-2"
-          >
-            <input
-              ref="emailInput"
-              v-model="form.email"
-              class="appearance-none mb-36 bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-              type="text"
-              name="email"
-              placeholder="your@email.com"
-              aria-label="Email address"
-            />
-
-            <button
-              class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
-              type="submit"
-            >
-              Send
-            </button>
-          </form>
-        </div>
-      </div>
+         <div class="contact-me" style="background-color: #f9f9f9; border: 1px solid #ddd; padding: 20px; border-radius: 5px; width: 300px; margin: 0 auto;">
+        <h2>Contact Me</h2>
+        <ul>
+            <li><strong>Email:</strong> <a href="mailto:khaledjr2018@gmail.com">khaledjr2018@gmail.com</a></li>
+            <li><strong>Telegram:</strong> <a href="https://t.me/jirou_fujimi">https://t.me/jirou_fujimi</a></li>
+        </ul>
+    </div>
+        
+        
       <div class="flex flex-col w-full xl:w-2/5">
         <img
           alt="Hero"
@@ -60,42 +40,6 @@ export default class Home extends Vue {
   get posts(): Post[] {
     return this.$store.state.posts;
   }
-
-  isSignedUp = false;
-
-  form = {
-    email: '',
-  };
-
-  encode(data): string {
-    return Object.keys(data)
-      .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-      .join('&');
-  }
-
-  validEmail(email): boolean {
-    // eslint-disable-next-line
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  }
-
-  async handleSubmit(): Promise<void> {
-    if (!this.validEmail(this.form.email)) {
-      this.$refs.emailInput.focus();
-      return;
-    }
-
-    try {
-      await fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: this.encode({ 'form-name': 'signups', ...this.form }),
-      });
-
-      this.isSignedUp = true;
-    } catch (error) {
-      console.error(error);
-    }
-  }
 }
+
 </script>
